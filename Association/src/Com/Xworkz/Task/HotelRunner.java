7e6e0a72dca9f11bde301659b2ae2c4edf472613
Chandana4518 <chandanad.xworkz@gmail.com>
2023-07-16 11:30:11 +0530

    Association of two classes

---
+++ Association/src/Com/Xworkz/Task/HotelRunner.java
@@ -0,0 +1,15 @@
+package Com.Xworkz.Task;
+
+public class HotelRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hotel hotel=new Hotel("Udupi","BTM",50,"Superier_Room");
+		hotel.printInfo();
+		
+		Hotel hotel1=new Hotel("ABC","Chittor",60,"Suite");
+		hotel1.printInfo();
+
+	}
+
+}
---
+++ Association/src/Com/Xworkz/Task/IplTeamRunner.java
@@ -0,0 +1,15 @@
+package Com.Xworkz.Task;
+
+public class IplTeamRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IplTeam team=new IplTeam("MI",11,2,5);
+		team.printInfo();
+		
+		IplTeam team1=new IplTeam("CSK",11,3,3);
+		team1.printInfo();
+
+	}
+
+}
